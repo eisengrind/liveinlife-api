140f009d36134d416885b7e4857d98e69db5d8b9
TheMysteriousVincent <agpheaddikilla@yahoo.de>
2019-05-19 22:29:53 +0200

    add serviceaccount complete json marshalling

--- pkg/apis/serviceaccount/key/cockroachdb/complete.go
+++ pkg/apis/serviceaccount/key/cockroachdb/complete.go
@@ -1,10 +1,26 @@
 package cockroachdb
 
 import (
+	"encoding/json"
+
 	"github.com/51st-state/api/pkg/apis/serviceaccount/key"
 )
 
 type complete struct {
 	key.Identifier
 	key.Incomplete
+}
+
+func (c *complete) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		GUID               string `json:"guid"`
+		ServiceAccountGUID string `json:"service_account_guid"`
+		Name               string `json:"name"`
+		Description        string `json:"description"`
+	}{
+		c.GUID(),
+		c.Data().ServiceAccountGUID,
+		c.Data().Name,
+		c.Data().Description,
+	})
 }
